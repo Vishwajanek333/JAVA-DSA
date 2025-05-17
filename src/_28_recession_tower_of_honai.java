d1f7195cdd483de4316d64b3b4bad49228b0803b
Vishwajanek <vishwajrevacollegeaiandds@gmail.com>
2025-05-17 23:15:13 +0530

    file

---
+++ src/_28_recession_tower_of_honai.java
@@ -0,0 +1,18 @@
+public class _28_recession_tower_of_honai {
+    public static void towerhonai(int n, String src, String helper, String dest){
+        if(n==1){
+            System.out.println("TRANSFER DISK"+n+"from "+src +"to"+dest);
+            return;
+        }
+
+        towerhonai(n-1,src,dest,helper);
+        System.out.println("TRANSFER DISK"+n+"from "+src +"to"+dest);
+        towerhonai(n-1,helper,src,dest);
+
+    }
+    public static void main(String args[]){
+        int n=3;
+        towerhonai(n,"S","H","D");
+    }
+}
+
