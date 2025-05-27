e202c5641bf1bbfa4255d7b18fa718e39c52c883
Vishwajanek <vishwajrevacollegeaiandds@gmail.com>
2025-05-27 10:17:17 +0530

    file

--- src/_1_main.java
+++ src/_1_main.java
@@ -1,5 +1,9 @@
 public class _1_main {
-     public static void main(String []args){
-         System.out.print(" hello words");
-     }
+    public static void main(String[] args) {
+        int num[] = {10, 20, 30, 40};
+        System.out.println(" length of array" + num.length);
+        for (int i = num.length - 1; i >=0; i--) {
+            System.out.print(num[i] + " ");
+        }
+    }
 }
